207be1b9d43147977d7f04c669c37499f83260ef
pedro-rothen <p.rothen@outlook.cl>
2024-07-23 18:31:36 -0400

    Basic project setup

---
+++ Movies/Presentation/Favorites/FavoritesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FavoritesViewController.swift
+//  Movies
+//
+//  Created by Pedro on 23-07-24.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController, UISearchResultsUpdating {
+
+    let searchController = UISearchController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Favorites"
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search"
+        searchController.searchBar.isHidden = false
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        definesPresentationContext = true
+        searchController.isActive = true
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        
+    }
+}
---
+++ Movies/Presentation/Movies/MoviesViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MoviesViewController.swift
+//  Movies
+//
+//  Created by Pedro on 23-07-24.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController, UISearchResultsUpdating {
+    let searchController = UISearchController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Movies"
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search"
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        definesPresentationContext = true
+        searchController.isActive = true
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        
+    }
+    
+    @IBAction func pushDetail(_ sender: Any) {
+        let destination = FavoritesViewController()
+        destination.hidesBottomBarWhenPushed = true
+        navigationController?.pushViewController(destination, animated: true)
+    }
+}
